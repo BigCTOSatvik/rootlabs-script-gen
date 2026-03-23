5c6f8740feb08d8e5a4a5b839306165a0ff81134
BigCTOSatvik <Kumarsatvik89@gmail.com>
2026-03-23 23:49:56 +0530

    Serve index.html on root route

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
+app.get("/", (req, res) => res.sendFile(require("path").join(__dirname, "index.html")));
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
